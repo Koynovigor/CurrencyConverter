c9db3c32a66a5600e62c1bf4f8ce15ab333173ac
L3on1kL <igorkoynov_2001@gmail.com>
2025-06-22 17:39:58 +0700

    fix schemas and icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -46,6 +46,11 @@ android {
     room {
         schemaDirectory("$projectDir/schemas")
     }
+    sourceSets["androidTest"].assets.srcDir("$projectDir/schemas")
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
 }
 
 dependencies {
